f201b7bae56409b7f1e1c206b8154f063fb6aa3e
Maryna Dubnianskaya <dubnianskaya.marina@gmail.com>
2022-01-17 18:39:44 +0200

    initial data

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return (
+      <div>
+        <p>here will be pictures</p>
+      </div>
+    );
+  }
+}
+
+export default App;
